6b133cb2a52d94e899a45cd4f87a8af5d03c4885
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-08-19 20:13:35 +0800

    Add "hover" effect

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement('div');
   container.appendChild(square);
 }
+
+const handleMouseEnter = (e) => {
+  e.target.classList.add('mouse-hover');
+};
+
+const squares = document.querySelectorAll('#container div');
+squares.forEach((square) => {
+  square.addEventListener('mouseenter', handleMouseEnter);
+});
